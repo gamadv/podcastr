b0dda8bb138c1e9e4086bfc5c2535465f627539a
Moacir Gama - gamadv <lealdev.py@gmail.com>
2021-04-29 22:14:53 -0300

    ft: import context and header/player components

---
+++ src/pages/_app.tsx
@@ -0,0 +1,23 @@
+import '../styles/global.scss';
+
+import Header from '../components/Header';
+import Player from '../components/Player';
+
+import styles from '../styles/app.module.scss';
+import { PlayerContextProvider } from '../context/PlayerContext';
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <PlayerContextProvider>
+      <div className={styles.appWrapper}>
+        <main>
+          <Header />
+          <Component {...pageProps} />
+        </main>
+        <Player />
+      </div>
+    </PlayerContextProvider>
+  );
+}
+
+export default MyApp;
